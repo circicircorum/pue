5a8c54cd7d4ffbf40f95dbfa1737e50769552cf2
circi <circi@gmail.com>
2023-11-29 07:43:06 +0000

    restyled some elements.

--- site.js
+++ site.js
@@ -15,6 +15,32 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Assume 'remote_data.csv' is in the same directory as index.js
+app.get('/gxb_remote_data', (req, res) => {
+  fs.readFile('public/gxb/remote_data.csv', 'utf8', (err, data) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Error loading data from server');
+      } else {
+          res.send(data);
+      }
+  });
+});
+
+app.post('/gxb_remote_data', express.text(), (req, res) => {
+  const data = req.body; // Assuming you're sending data in the request body
+  console.log(req.body);
+  
+  fs.writeFile('public/gxb/remote_data.csv', `${data}\n`, (err) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Error saving data to remote_data.csv');
+      } else {
+          res.send('Data saved successfully!');
+      }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Pue listening on port ${port}...`)
 })
