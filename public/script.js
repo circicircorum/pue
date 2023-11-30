fa6e08ea15e706dd21038307ac791819919be0a8
circi <circi@gmail.com>
2023-11-30 14:38:31 +0000

    updated files

--- public/script.js
+++ public/script.js
@@ -3,6 +3,9 @@
 function getAii() {
     gxbText = "Miscellaneous Remarks: " + document.getElementById('gxbDataInput').value;
 
+    qav = document.getElementById('qav');
+    title = "Title: " + qav.getElementsByTagName('textarea')[0].value + "\n";
+
     otb = document.getElementById('otb');
     otbRizList = Array.from(otb.getElementsByClassName('riz'));
     dbq = document.getElementById('dbq');
@@ -16,8 +19,8 @@ function getAii() {
         rizAnswer = "Answer: " + rizList[i].getElementsByTagName('textarea')[0].value;
         rizzes += rizQuestion + "\n" + rizAnswer + "\n";
     }
-    
-    concatText = rizzes + gxbText;
+
+    concatText = title + rizzes + gxbText;
 
     return concatText;
 }
@@ -36,31 +39,16 @@ function saveToLocal() {
     link.click();
 }
 
-function downloadRemote() {
-    // Assuming you have a server-side endpoint for downloading data
-    // You can use fetch or any other method to download data from the server
-    // Example using fetch:
-    fetch('/gxb_remote_data')
-        .then(response => response.text())
-        .then(data => {
-            const blob = new Blob([data], { type: 'text/csv' });
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'remote_data.csv';
-            link.click();
-        });
-}
-
 function loadFromFile() {
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = '.csv';
 
-    input.addEventListener('change', function() {
+    input.addEventListener('change', function () {
         const file = this.files[0];
         const reader = new FileReader();
 
-        reader.onload = function() {
+        reader.onload = function () {
             //document.getElementById('gxbDataInput').value = reader.result;
             setAii(reader.result);
         };
@@ -72,9 +60,6 @@ function loadFromFile() {
 }
 
 function loadFromServer() {
-    // Assuming you have a server-side endpoint for fetching data
-    // You can use fetch or any other method to get data from the server
-    // Example using fetch:
     fetch('/gxb_remote_data')
         .then(response => response.text())
         .then(data => {
@@ -88,10 +73,7 @@ function loadFromServer() {
 function saveToRemote() {
     /*const data = document.getElementById('gxbDataInput').value;*/
     const data = getAii();
-
-    // Assuming you have a server-side endpoint for saving data remotely
-    // You can use fetch or any other method to send data to the server
-    // Example using fetch:
+    
     fetch('/gxb_remote_data', {
         method: 'POST',
         /*
@@ -101,31 +83,24 @@ function saveToRemote() {
         //*/
         body: data//'test-str-r'//data//JSON.stringify(data)
     })
-    .then(response => response.text())
-    .then(result => {
-        //document.getElementById('responseText').textContent += ' ' + result;
-        const row = document.createElement('div');
-        row.textContent = result;
-        document.getElementById('gxbResponseText').appendChild(row);
-    });
+        .then(response => response.text())
+        .then(result => {
+            //document.getElementById('responseText').textContent += ' ' + result;
+            const row = document.createElement('div');
+            row.textContent = result;
+            document.getElementById('gxbResponseText').appendChild(row);
+        });
 }
 
-
-/* Otb and Dbq */
-// otbGwd = document.getElementById('otb-gwd');
-// otbGwd.addEventListener("load", otber);
-// otber();
-
-// body = document.getElementsByTagName("body");
-window.onload = function() {
+window.onload = function () {
     otber();
     dbqer();
 };
 
 function otber() {
     prompts = ["What did you do today?",
-                "How did you feel?",
-                "When did you sleep?"];
+        "How did you feel?",
+        "When did you sleep?"];
     for (promptIndex in prompts) {
         promptQuestionAndAnswerDiv = document.createElement('div');
         promptQuestionAndAnswerDiv.className += "riz";
@@ -138,11 +113,10 @@ function otber() {
     }
 }
 
-//dbqer();
 function dbqer() {
     prompts = ["How was the weather?",
-                "What did you learn?",
-                "Any remarks?"];
+        "What did you learn?",
+        "Any remarks?"];
     for (promptIndex in prompts) {
         promptQuestionAndAnswerDiv = document.createElement('div');
         promptQuestionAndAnswerDiv.className += "riz";
